8554969c7f5bb718637d028161d0f968d11e2da9
Zubaydullo Abdirakhmonov <zubaydullo0022@gmail.com>
2023-09-25 20:11:19 +0400

    refactor code

--- 4A-Watermelon/main.c
+++ 4A-Watermelon/main.c
@@ -8,17 +8,11 @@ int main(void)
     if (w >= 1 && w <= 100)
     {
         if (w == 2)
-        {
             printf("NO\n");
-        }
         else if (w % 2 == 0)
-        {
             printf("YES\n");
-        }
         else
-        {
             printf("NO\n");
-        }
     }
     return (0);
 }
