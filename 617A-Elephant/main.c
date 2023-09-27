301f998653846af46eeed97302816d9018222761
Zubaydullo Abdirakhmonov <zubaydullo0022@gmail.com>
2023-09-28 00:48:30 +0400

    feat: Elephant problem done

--- 617A-Elephant/main.c
+++ 617A-Elephant/main.c
@@ -2,6 +2,39 @@
 
 int main(void)
 {
-    
-    return (0);
+    int x;
+    scanf("%d", &x);
+
+    int steps = 0;
+    while (x > 0)
+    {
+        if (x >= 5)
+        {
+            x -= 5;
+            steps++;
+        }
+        else if (x >= 4)
+        {
+            x -= 4;
+            steps++;
+        }
+        else if (x >= 3)
+        {
+            x -= 3;
+            steps++;
+        }
+        else if (x >= 2)
+        {
+            x -= 2;
+            steps++;
+        }
+        else
+        {
+            x -= 1;
+            steps++;
+        }
+    }
+
+    printf("%d\n", steps);
+    return 0;
 }
