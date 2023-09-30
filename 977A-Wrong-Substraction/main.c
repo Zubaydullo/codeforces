06dc6446eba62e8fc34b438b21907b408c1a265b
Zubaydullo Abdirakhmonov <zubaydullo0022@gmail.com>
2023-09-30 11:33:48 +0400

    feat: Wrong Substraction problem done

---
+++ 977A-Wrong-Substraction/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int n, k, number;
+
+    scanf("%d %d", &n, &k);
+
+    number = n;
+    for (int i = 0; i < k; i++)
+    {
+        if (number % 10 > 0)
+        {
+            number--;
+        }
+        else if (number % 10 == 0)
+        {
+            number = number / 10;
+        }
+    }
+    printf("%d", number);
+    return (0);
+}
